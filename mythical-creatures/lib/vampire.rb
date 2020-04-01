d4eab3449f1be996329e42bce1d039310dd462aa
Nicola Kim <nicola.copsey@gmail.com>
2020-03-31 17:11:54 -0700

    Vampire Test and Class complete

--- mythical-creatures/lib/vampire.rb
+++ mythical-creatures/lib/vampire.rb
@@ -1,1 +1,20 @@
 require 'pry'
+
+class Vampire
+  attr_reader :name, :pet, :thirsty
+
+  def initialize(name, pet="bat")
+    @name = name
+    @pet = pet
+    @thirsty = true
+  end
+
+  def thirsty?
+    @thirsty
+  end
+
+  def drink
+    @thirsty = false
+  end
+
+end
