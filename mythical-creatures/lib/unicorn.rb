240aef872a913ee537faa021b9bab326e2167736
Nicola Kim <nicola.copsey@gmail.com>
2020-03-31 17:05:47 -0700

    Unicorn class and tests complete

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -1,1 +1,23 @@
 require 'pry'
+
+class Unicorn
+  attr_reader :name, :color
+
+  def initialize(name, color="white")
+    @name = name
+    @color = color
+  end
+
+  def white?
+    if @color == "white"
+      true
+    else
+      false
+    end
+  end
+
+  def say(words)
+    p "**;* #{words} **;*"
+  end
+
+end
