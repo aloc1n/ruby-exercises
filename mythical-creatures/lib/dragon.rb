f2b5d80345ea038f03cbd463908d4cdeffa6226f
Nicola Kim <nicola.copsey@gmail.com>
2020-03-31 17:25:27 -0700

    Dragon class and test complete

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -1,1 +1,23 @@
 require 'pry'
+
+class Dragon
+  attr_reader :name, :rider, :color
+
+  def initialize(name, color, rider)
+    @name = name
+    @rider = rider
+    @color = color
+    @hungry = true
+    @food_count = 0
+  end
+
+  def hungry?
+    @hungry
+  end
+
+  def eat
+    @food_count += 1
+    @hungry = false if @food_count == 3
+  end
+
+end
