04d129825095d11ca5dcd40f71b9cc2ff459a6c1
Nicola Kim <nicola.copsey@gmail.com>
2020-04-05 09:28:57 -0700

    Wizard class and tests complete

---
+++ mythical-creatures/lib/wizard.rb
@@ -0,0 +1,27 @@
+class Wizard
+  attr_reader :name, :bearded, :spell_count
+
+  def initialize(name, bearded={:bearded => true})
+    @name = name
+    @bearded = bearded
+    @spell_count = 0
+  end
+
+  def bearded?
+    @bearded.value?(true)
+  end
+
+  def incantation(root_power)
+    p "sudo #{root_power}"
+  end
+
+  def rested?
+    @spell_count <= 2
+  end
+
+  def cast
+    @spell_count += 1
+    p "MAGIC MISSILE!"
+  end
+
+end
