02039630269b6cf31e6f36dfea378a2a878341aa
Nicola Kim <nicola.copsey@gmail.com>
2020-04-06 07:45:08 -0700

    Leather chair class and tests complete

--- command-query/exercises/lib/leather_chair.rb
+++ command-query/exercises/lib/leather_chair.rb
@@ -1,10 +1,15 @@
 class LeatherChair
+  attr_reader :faded
+  
   def initialize
+    @faded = faded
   end
 
   def faded?
+    @faded
   end
 
   def expose_to_sunlight
-  end 
+    @faded = "Exposed chairs are faded."
+  end
 end
